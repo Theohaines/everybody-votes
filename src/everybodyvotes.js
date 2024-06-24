9d739b9efb3b8e85d93742c3016a1efb440cb6d6
theohaines <theohaines@yahoo.com>
2024-06-24 18:55:22 +0100

    add server listen

--- src/everybodyvotes.js
+++ src/everybodyvotes.js
@@ -3,3 +3,11 @@ const session = require('express-session');
 const dotenv = require('dotenv').config();
 
 const app = express();
+
+app.get("/", (req, res) => {
+    res.send("FUCK");
+});
+
+app.listen(process.env.PORT, () => {
+
+});
